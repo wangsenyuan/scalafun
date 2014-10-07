47d7c7ccefae1b62051e4d1e5fac74f8f7e52347
wangsenyuan <wang.senyuan@gmail.com>
2014-10-07 21:56:07 +0800

    complete tree example

---
+++ src/s99/Tree.scala
@@ -0,0 +1,33 @@
+package s99
+
+/**
+ * Created by senyuanwang on 14-10-7.
+ */
+sealed abstract class Tree[+T]
+
+case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
+  override def toString = "T(" + value.toString + " " + left.toString + " " + right.toString + ")"
+}
+
+case object End extends Tree[Nothing] {
+  override def toString = "."
+}
+
+object Node {
+  def apply[T](value: T): Node[T] = Node(value, End, End)
+}
+
+object Tree {
+  def cBalanced[T](nodes: Int, value: T): List[Tree[T]] = nodes match {
+    case n if n < 1 => List(End)
+    case n if n % 2 == 1 => {
+      val subtrees = cBalanced(n / 2, value)
+      subtrees.flatMap(l => subtrees.map(r => Node(value, l, r)))
+    }
+    case n if n % 2 == 0 => {
+      val lesserSubtrees = cBalanced((n - 1) / 2, value)
+      val greaterSubtrees = cBalanced((n - 1) / 2 + 1, value)
+      lesserSubtrees.flatMap(l => greaterSubtrees.flatMap(g => List(Node(value, l, g), Node(value, g, l))))
+    }
+  }
+}
---
+++ src/s99/TreeApp.scala
@@ -0,0 +1,11 @@
+package s99
+
+/**
+ * Created by senyuanwang on 14-10-7.
+ */
+object TreeApp extends App {
+
+  import s99.Tree._
+
+  println(cBalanced(4, 'x))
+}
